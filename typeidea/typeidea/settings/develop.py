c7df55a899f75036a0ac76ebfe992de8b3229bf3
fanglin-jin <1343419328@qq.com>
2020-12-02 21:48:53 +0800

    Initial commit

---
+++ typeidea/typeidea/settings/develop.py
@@ -0,0 +1,13 @@
+from .base import *  # NOQA
+
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+Debug = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
